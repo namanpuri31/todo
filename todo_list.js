6baa8ae63fd76f2dba117f18fa2d81fbe7e54adf
namanpuri31 <113339416+namanpuri31@users.noreply.github.com>
2022-09-27 19:23:47 +0530

    Add files via upload
    added

---
+++ todo_list.js
@@ -0,0 +1,15 @@
+let add=document.getElementById("additems")
+let lstitem=document.getElementById("listitems")
+let itemtodo=document.getElementById("itemstodo")
+
+add.addEventListener("click",()=>{
+    let items=document.createElement("ul")
+    items.classList.add("styling")
+    items.innerText=`${lstitem.value}`
+    itemtodo.appendChild(items)
+    lstitem.value=""
+    items.addEventListener("click",()=>{
+        itemtodo.removeChild(items)
+    })
+    
+})
